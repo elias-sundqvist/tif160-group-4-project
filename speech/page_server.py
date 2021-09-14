8819401e60f223ddc643bf882453bed56a3d1ec7
Elias Sundqvist <elias@sundqvist.com>
2021-09-14 14:57:00 +0200

    Rapberry pi support

--- speech/page_server.py
+++ speech/page_server.py
@@ -4,18 +4,15 @@ from http.server import HTTPServer, CGIHTTPRequestHandler
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-chromedir= "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
-
-CHROMEDRIVER_PATH = os.getcwd()+"/chromedriver.exe";
+CHROMEDRIVER_PATH = "/usr/lib/chromium-browser/chromedriver";
 options = Options()
 options.add_experimental_option("prefs", {
     "profile.default_content_setting_values.media_stream_mic": 1
   })
-#options.headless = True
 options.add_argument("--use-fake-ui-for-media-stream")
 options.add_argument("--disable-user-media-security")
+options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
-driver.minimize_window()
 
 def serve_page():
     server_object = HTTPServer(server_address=('', 1654), RequestHandlerClass=CGIHTTPRequestHandler)
