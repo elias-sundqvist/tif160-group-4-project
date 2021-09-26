3de79187dee884f941819c3d7888a94b81b00133
Elias Sundqvist <elias@sundqvist.com>
2021-09-26 13:15:01 +0200

    Add radian/servo conversion functions

--- serial_communication/preproccesing.py
+++ serial_communication/preproccesing.py
@@ -1,3 +1,5 @@
+from math import pi
+
 # Servo ID's
 BODY      = '0'
 NECK_PAN  = '1'
@@ -14,8 +16,27 @@ MIN_MAX_VALUES = {BODY:[560, 2330],
                   ELBOW:[550, 2340],
                   GRIP:[950, 2400]}
 
+# SERVO_FOR_MIN_RAD, SERVO_FOR_MAX_RAD, MIN_RAD, MAX_RAD
+MIN_MAX_SERVO_RAD = {
+    BODY: [950,1900,-pi/4,pi/4],
+    SHOULDER: [2180, 1280, 0, pi/2],
+    ELBOW: [1400, 2400, 0, pi/2],
+    NECK_PAN: [550, 2300, -pi/2, pi/2],
+    NECK_TILT: [2350, 1000, -pi/4, pi/2]
+}
+
 GRIPPER_VALUES = {'red':1160,
                   'blue':1250}
+
+def servo_to_rad(servo, servo_value):
+    min_serv, max_serv, min_rad, max_rad = MIN_MAX_SERVO_RAD[servo]
+    a = (servo_value - max_serv)/(min_serv - max_serv)
+    return a * (min_rad - max_rad) + max_rad
+
+def rad_to_servo(servo, radians):
+    min_serv, max_serv, min_rad, max_rad = MIN_MAX_SERVO_RAD[servo]
+    a = (radians - max_rad)/(min_rad - max_rad)
+    return int(a * (min_serv - max_serv) + max_serv)
 
 def body_to_deg(body_servo):
     m = 9970/7
