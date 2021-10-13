99d5c539e2911480dcc433077f5dc0d1b09fbd4d
David Näslund <davidna@net.chalmers.se>
2021-10-13 16:44:56 +0200

    added diffsteering to ids and tasks

--- Differential Steering/Hubert_Differential.ino
+++ Differential Steering/Hubert_Differential.ino
@@ -11,63 +11,159 @@
 //
 //  Adapted from a sketch by zoomkat 10-22-11 serial servo test
 
-#include <Servo.h>                      //  You need to include Servo.h as it is used by the HB-25 Library
+
+//  Valid speed ranges for the forwardAtSpeed and reverseAtSpeed methods are
+//  0 (stop) to 500 (maximum speed). For rampToSpeed and moveAtSpeed you can use from -500 (full
+//  reverse) to 500 (full forward). As before, a speed of 0 will stop the motor.
+//
+//  Remember to call the begin() method in setup().
+
+
+#include <Servo.h>                    //You need to include Servo.h as it is used by the HB-25 Library
 #include <HB25MotorControl.h>
+#include <TimedAction.h>
 
 
-#define INPUT_SIZE 16 // The size of char's from Pi    (my guess 2*8)
+#define INPUT_SIZE 24                 //The size of char's from Pi
 char input[INPUT_SIZE + 1];
 
-#define leftWheel_id 0
-#define rightWheel_id 1
-const byte controlPin = {2,3};              //  Pin Definition
-const int speed_init[] = {0,0};
+#define leftWheel_id 7
+#define rightWheel_id 8
+#define timeStamp_id 9
 
-int curr_speed[2];
-int new_speed[2];
+int curr_speedL;
+int curr_speedR;
+int new_speedL;
+int new_speedR;
+int curr_time = 0;
 
-const int speed_min[] = {-500, -500};
-const int speed_max[] = {500, 500};
+int leftSpeed, rightSpeed;
 
-HB25MotorControl motorControlL(controlPin(leftWheel_id));
-HB25MotorControl motorControlR(controlPin(rightWheel_id));
+const int speed_min = -500;
+const int speed_max = 500;
+
+HB25MotorControl motorControlL(2);
+HB25MotorControl motorControlR(3);
+
+
+void sendServoData(){  
+    // Might change this to not send a string
+    String data = String(leftWheel_id)+':'+String(curr_speedL)+'&'+
+                  String(rightWheel_id)+':'+String(curr_speedR)+'&'+
+                  String(timeStamp_id)+':'+String(curr_time);
+    
+    Serial.println(data);
+}
+
+
+//Create a couple timers that will fire repeatedly every x ms
+TimedAction writeThread = TimedAction(1,sendServoData);
 
 
 //Servo update function
-void servo_speed(const int new_speed, const int id) {
-
-  int diff, steps, now, CurrVal, NewVal, delta = 6;
-
-  //current servo value
-  now = curr_speed[id];
-  CurrVal = now;
-  NewVal = new_speed;
-
-  /* determine interation "diff" from old to new position */
-  diff = (NewVal - CurrVal)/abs(NewVal - CurrVal); // Should return +1 if NewPwm is bigger than CurrPwm, -1 otherwise.
-  steps = abs(NewVal - CurrVal);
+void servo_speed(const int new_speedL, const int new_speedR) {
+  
+  int diffL, stepsL, nowL, CurrValL, NewValL, delta = 6,
+      diffR, stepsR, nowR, CurrValR, NewValR, steps; 
+  
+  nowL = curr_speedL;
+  nowR = curr_speedR;
+  CurrValL = nowL;
+  CurrValR = nowR;
+  NewValL = new_speedL;
+  NewValR = new_speedR;
+  
+  // Should return +1 if NewPwm is bigger than CurrPwm, -1 otherwise.
+  diffL = (NewValL - CurrValL)/abs(NewValL - CurrValL); 
+  diffR = (NewValR - CurrValR)/abs(NewValR - CurrValR);
+  stepsL = abs(NewValL - CurrValL);
+  stepsR = abs(NewValR - CurrValR);
   delay(10);
 
-  for (int i = 0; i < steps; i += delta) {
-    now = now + delta*diff;
-    if(now > speed_max[id]){
-      now = speed_max[id];
-    }
-    else if(now < pos_min[id]){
-      now = pos_min[id];
-    }
-    if(id==leftWheel_id{
-      motorControlL.moveAtSpeed(speed_init[leftWheel_id]);
-    }
-    else if(id==rightWheel_id){
-      motorControlR.moveAtSpeed(speed_init[rightWheel_id]);
-    }
-    curr_speed[id] = now;
+  steps = (stepsL>=stepsR) * stepsL + (stepsR>stepsL) * stepsR;
+  
+  for (int i = 0; i < steps+1; i += delta) {
+    
+    nowL += (i<=stepsL) * delta*diffL;
+    nowR += (i<=stepsR) * delta*diffR;
+
+    nowL = (nowL<speed_max) ? nowL : speed_max;
+    nowR = (nowR<speed_max) ? nowR : speed_max;
+    
+    nowL = (nowL>speed_min) ? nowL : speed_min;
+    nowR = (nowR>speed_min) ? nowR : speed_min;
+    
+    nowL = (abs(new_speedL-nowL)<=delta) ? new_speedL : nowL;
+    nowR = (abs(new_speedR-nowR)<=delta) ? new_speedR : nowR;
+    
+    motorControlL.moveAtSpeed(nowL);
+    motorControlR.moveAtSpeed(nowR);
+
+    curr_speedL = nowL;
+    curr_speedR = nowR;
+    
     writeThread.check();
     delay(20);
   }
+
+  if(new_speedL==curr_speedL && new_speedR==curr_speedR && curr_time>0) {
+      int t = curr_time;
+      for(int i = 0; i < t; i++){
+          motorControlL.moveAtSpeed(curr_speedL);
+          motorControlR.moveAtSpeed(curr_speedR);
+          
+          curr_time += -1;
+          curr_time = (curr_time>0) * curr_time;
+          
+          writeThread.check();
+          delay(50);
+      }
+  }
+}
+
+
+void setup() {
+  Serial.begin(57600); // Starts the serial communication
+
+    motorControlL.begin();
+    motorControlL.moveAtSpeed(0);
+    
+    motorControlR.begin();
+    motorControlR.moveAtSpeed(0);
+
+    //Initilize curr_pos and new_servo_val vectors
+    curr_speedL = 0;
+    curr_speedR = 0;
+    new_speedL = curr_speedL;
+    new_speedR = curr_speedR;
+
+    delay(2000);
+}
+
+
+void loop() {
+
+  byte size = Serial.readBytes(input, INPUT_SIZE);  //Might need a buffer if the Pi just spam the Arduino with commands
+  input[size] = 0;                                  //Add the final 0 to end the C string
   
-  delay(10);
+  
+  char* command = strtok(input, "&");
+  while(command!=0){
+
+      int id = atoi(command);
+      int val = atoi(strchr(command, ':')+1);
+
+      leftSpeed  = (id==leftWheel_id)  ? val : leftSpeed;
+      rightSpeed = (id==rightWheel_id) ? val : rightSpeed;
+      curr_time  = (id==timeStamp_id)  ? val : curr_time;
+      
+      command = strtok(0, "&");
+  }
+
+  leftSpeed  = (curr_time>0) * leftSpeed;
+  rightSpeed = (curr_time>0) * rightSpeed;
+  
+  servo_speed((const int) leftSpeed, (const int) rightSpeed);
 }
 
 
--- agent/tasks.py
+++ agent/tasks.py
@@ -1,23 +1,23 @@
 import torch
 
-from agent.agent import Agent
+#from agent.agent import Agent
 from serial_communication.servo_utils import servo_to_rad, rad_to_servo
 from serial_communication.servo_ids import *
 import kinematics.kinematics as k
 from math import pi
 
 class Task:
-    def run(self, agent: Agent, current_pose: dict):
+    def run(self, current_pose: dict):
         pass
 
-    def is_done(self, agent: Agent, current_pose: dict):
+    def is_done(self, current_pose: dict):
         return True
 
 class WaitForInstructions(Task):
-    def run(self, agent: Agent, current_pose: dict):
+    def run(self, current_pose: dict):
         pass
 
-    def is_done(self, agent: Agent, current_pose: dict):
+    def is_done(self, current_pose: dict):
         return True
 
 class MoveHandToPosition(Task):
@@ -31,7 +31,7 @@ class MoveHandToPosition(Task):
             servo_to_rad(ELBOW, current_pose[ELBOW])
         ]
 
-    def run(self, agent: Agent, current_pose: dict):
+    def run(self, current_pose: dict):
         new_dict = {}
 
         body_rad, shoulder_rad, elbow_rad = k.step_towards_target(self.get_joint_angles(current_pose), self.target, 50, 0.9)
@@ -41,7 +41,7 @@ class MoveHandToPosition(Task):
 
         return new_dict
 
-    def is_done(self, agent: Agent, current_pose: dict):
+    def is_done(self, current_pose: dict):
         with torch.no_grad:
             pos = k.forward(self.get_joint_angles(current_pose))
             distance = torch.mean(torch.square(torch.sub(pos, self.target)))
@@ -61,5 +61,17 @@ class CloseGrip(Task):
     def is_done(self, agent, current_pose):
         return agent.is_in_pose(current_pose, {GRIP: 950})
     
+class SetWheelSpeeds(Task):
+    def __init__(self, leftSpeed, rightSpeed, duration):
+        self.targetSpeed = [leftSpeed, rightSpeed, duration]
+        
 
+    def run(self, current_speeds: dict):
+        new_dict = {}
+
+        new_dict[LEFT_WHEEL] = self.targetSpeed[0]
+        new_dict[RIGHT_WHEEL] = self.targetSpeed[1]
+        new_dict[SPEED_DUR] = self.targetSpeed[2]
+
+        return new_dict
     
