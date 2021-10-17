a888eed0b1a44db5fed2b42445990627529e08cd
DavidNas <61546841+Awaski@users.noreply.github.com>
2021-10-17 17:48:40 +0200

    Update test.py

--- cascade_classifier/test.py
+++ cascade_classifier/test.py
@@ -57,15 +57,9 @@ while(True):
                 speedCorrection = handWidthFactor - h/frameWidth
             break
         
-            
-
-    # Display the resulting frame
-    cv2.imshow('preview',frame)
-
-    #Waits for a user input to quit the application
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+
+
