e0bb410c51f144c3110e44c4231719f3189dc66e
DavidNas <61546841+Awaski@users.noreply.github.com>
2021-10-17 17:25:28 +0200

    Update test.py
    added variables for controlling the diff-steering, speed and camera-tilt for finding the hand

--- cascade_classifier/test.py
+++ cascade_classifier/test.py
@@ -15,13 +15,18 @@ minNeighbors_max = 40
 targetWaitFrames = 3
 targetCountdown = targetWaitFrames
 
+frameHeight = 1080
+frameWidth = 1920
 
+handWidthFactor = 0.3
+
+steerCorrection = 0
+tiltCorrection = 0
+speedCorrection = 0
 while(True):
 
     # Capture frame-by-frame
     _, frame = cap.read()
-
-
 
     hands = hand_cascade.detectMultiScale(image = frame, scaleFactor=1.2, minSize = (10,10), maxSize = (200,200), minNeighbors=minNeighbors)
 
@@ -43,9 +48,13 @@ while(True):
         targetCountdown -=1
         if (targetCountdown < 0):
             targetCountdown = 0
-        for (x,y,w,h) in hands:
-            cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0),2)
-
+            for (x,y,w,h) in hands:
+                cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0),2)
+                xCenter = x+w/2
+                yCenter = x+h/2
+                steeringCorrection = xCenter-frameWidth/2
+                tiltCorrection = yCenter-frameHeight/2
+                speedCorrection = handWidthFactor - h/frameWidth
     
 
     # Display the resulting frame
